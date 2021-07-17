d0573e1f18f1a4c0f442bd8625c5c16f373f78cf
cjsssusa <cjsssusa@gmail.com>
2021-07-17 08:13:53 -0400

    cjs first push

---
+++ src/main/java/com/mukundmadhav/springboot/springboot/dao/EmployeeDAO.java
@@ -0,0 +1,13 @@
+package com.mukundmadhav.springboot.springboot.dao;
+import java.util.List;
+import com.mukundmadhav.springboot.springboot.modal.Employee;
+public interface EmployeeDAO {
+ 
+ List<Employee> get();
+ 
+ Employee get(int id);
+ 
+ void save(Employee employee);
+ 
+ void delete(int id);
+}
